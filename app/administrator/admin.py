2d43715831b59374d9edfb3cd04af554ed2db03a
dickqi <dickqi87@gmail.com>
2019-12-14 23:05:48 +0800

    add admin

--- app/administrator/admin.py
+++ app/administrator/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Administrator
+
 
 # Register your models here.
+
+@admin.register(Administrator)
+class AdministratorAdmin(admin.ModelAdmin):
+    list_per_page = 50
--- app/book/admin.py
+++ app/book/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Book
+
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_per_page = 50
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_per_page = 50
