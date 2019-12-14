94106e52f0f871fd660fa75373fff646bf3e1232
dickqi <dickqi87@gmail.com>
2019-12-14 22:49:27 +0800

    change sql

--- AiReadingManageBook/settings.py
+++ AiReadingManageBook/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'AiReadingManageBook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "AiReading",
+        "USER": "root",
+        "PASSWORD": "macbook123456",
+        "HOST": "localhost",
+        "PORT": 3306
     }
 }
 
